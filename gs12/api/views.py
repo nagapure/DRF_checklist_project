573e146ada0591a2f4598be30a23a599e261a6d4
nagapure <nagapure50@gmail.com>
2022-02-01 17:19:39 +0530

    Added BasisAuthentication using permission classes

---
+++ gs12/api/views.py
@@ -0,0 +1,22 @@
+# GenericAPIView and Model Mixin
+from tkinter import N
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
+    #  permission_classes = [AllowAny]
+    permission_classes = [IsAdminUser]
+
+
+
+
+
