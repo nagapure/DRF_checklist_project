be92621ad1113a356f373bba4e4d67882202d54c
nagapure <nagapure50@gmail.com>
2022-02-02 12:42:24 +0530

    Added SessionAuthentication permissin

--- gs13/api/views.py
+++ gs13/api/views.py
@@ -5,13 +5,13 @@ from .serializers import StudentSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions
 
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    authentication_classes = [BasicAuthentication]
+    authentication_classes = [SessionAuthentication]
 
     # In IsAuthenticated all the authenticated user will be able to access the data
     # permission_classes = [IsAuthenticated]
@@ -19,7 +19,16 @@ class StudentModelViewSet(viewsets.ModelViewSet):
     #  permission_classes = [AllowAny]
 
     # When we use IsAdminUser, then only user can access the data whose staf status is true
-    permission_classes = [IsAdminUser]
+    # permission_classes = [IsAdminUser]
+    
+    # If the user is authenticated then he can make changes in app if not he can only able to read data
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+
+    permission_classes = [DjangoModelPermissions]
+
+
+
+      
 
     
 
